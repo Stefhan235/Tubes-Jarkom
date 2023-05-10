f427ea8d11574e51072a76868b771415879bed00
Stefhan Salim <110758753+Stefhan235@users.noreply.github.com>
2023-05-10 23:56:03 +0700

    Delete tcp_client.py

--- tcp_client.py
+++
@@ -1,8 +0,0 @@
-from socket import *
-
-serverName = "localhost"
-serverPort = 80
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-
-clientSocket.close()
